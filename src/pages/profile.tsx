ec4cf725f83e86d933ff0bf0a7cd08896cb68dbf
StipGod <72163911+StipGod@users.noreply.github.com>
2023-04-13 15:30:44 -0500

    GET implemented in code
    GET implemented on the main profile page, and fixed some stuff regarding the edit page. Ps: Edit page still needs to be updated with being able to upload the img.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,82 +1,78 @@
 import { Box, Flex, Heading, Text, Image, Stack, Link } from '@chakra-ui/react'
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { PageLayout } from '@/components/pageLayout';
 
-axios.get('/api/getprofile')
-    .then((response) => {
-        const items = response.data;
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+export default function Profile() {
+    const [items, setItems] = useState({});
 
-const socialMedia = ["Twitter", "Instagram", "YouTube"];
+    useEffect(() => {
+        axios.get('/api/getprofile', {
+            params: {
+              email: "stipgod2@gmail.com"
+            }
+          })
+          .then(function (response) {
+            console.log(response);
+            setItems(response.data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    }, []);
 
-
-type ProfileProps = {
-    fullName: string;
-    email: string;
-    cellphone: string;
-    profilePicture: string;
-    bio: string;
-    twitter: string;
-    instagram: string;
-    youtube: string;
-};
-
-const Profile = ({ fullName, email, cellphone, profilePicture, bio, twitter, instagram, youtube }: ProfileProps) => {
     return (
-        <Flex flexDirection="column" alignItems="center" bg="#2D4785" color="white" py="5" px="3">
-            <Image
-                src={profilePicture}
-                alt="Profile Picture"
-                width="150px"
-                height="150px"
-                borderRadius="full"
-                objectFit="cover"
-                mb="3"
-                boxShadow="md"
-            />
-            <Text fontSize="2xl" fontWeight="bold" textAlign="center">
-                {fullName}
-            </Text>
-            <Text fontSize="md" color="#526081" textAlign="center" mb="3">
-                {email} &#8226; {cellphone}
-            </Text>
-            <Text textAlign="center" px="10" mb="5">
-                {bio}
-            </Text>
-            <Stack isInline spacing="2" mb="3">
-                <Link
-                    fontSize="lg"
-                    color="white"
-                    href={`https://twitter.com/${twitter}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Twitter
-                </Link>
-                <Link
-                    fontSize="lg"
-                    color="white"
-                    href={`https://instagram.com/${instagram}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Instagram
-                </Link>
-                <Link
-                    fontSize="lg"
-                    color="white"
-                    href={`https://youtube.com/${youtube}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    YouTube
-                </Link>
-            </Stack>
-        </Flex>
+        <PageLayout>
+            <Flex flexDirection="column" alignItems="center" bg="#2D4785" color="white" py="5" px="3">
+                <Image
+                    src={items.profilePicture}
+                    alt="Profile Picture"
+                    width="150px"
+                    height="150px"
+                    borderRadius="full"
+                    objectFit="cover"
+                    mb="3"
+                    boxShadow="md"
+                />
+                <Text fontSize="2xl" fontWeight="bold" textAlign="center">
+                    {items.name}
+                </Text>
+                <Text fontSize="md" color="#526081" textAlign="center" mb="3">
+                    {items.email}  {items.cellphone}
+                </Text>
+                <Text textAlign="center" px="10" mb="5">
+                    {items.description}
+                </Text>
+                <Stack isInline spacing="2" mb="3">
+                    <Link
+                        fontSize="lg"
+                        color="white"
+                        href={`${items.twitter}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Twitter
+                    </Link>
+                    <Link
+                        fontSize="lg"
+                        color="white"
+                        href={`${items.instagram}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Instagram
+                    </Link>
+                    <Link
+                        fontSize="lg"
+                        color="white"
+                        href={`${items.youtube}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        YouTube
+                    </Link>
+                </Stack>
+            </Flex>
+        </PageLayout>
     );
 };
-
-export default Profile;
