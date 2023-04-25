e819d34b1203515511f013088e9a69977af7bcdf
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-04-25 10:43:30 -0500

    make search
    users can now search through the listings titles.

--- src/pages/api/getListings.ts
+++ src/pages/api/getListings.ts
@@ -7,11 +7,11 @@ export default async function handler(
 ) {
     if (req.method == "POST") {
         const query = req.body.query; 
-        console.log(query)
         try {
             const client = await clientPromise;
             const db = client.db("wello");
-            const dbQuery = db.collection("listings").find();
+            await db.collection("listings").createIndex({ title:  "text" });
+            const dbQuery = db.collection("listings").find({ $text: { $search: query } });
             const listings = await dbQuery.toArray();
 
             res.status(200).json({ statusCode: 200, message: "Succes",listings : listings })
