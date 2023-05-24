80fe9eefbea8d911c2cba3cc62b7f10dd4bf2753
StipGod <72163911+StipGod@users.noreply.github.com>
2023-05-24 16:53:25 -0500

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,10 +12,6 @@ export const authOptions = {
         clientId: process.env.GITHUB_ID as string,
         clientSecret: process.env.GITHUB_SECRET as string,
       }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID as string,
-        clientSecret: process.env.GOOGLE_SECRET as string,
-      }),
     ],
     secret : process.env.NEXTAUTH_SECRET,
   }
